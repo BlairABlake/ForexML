d060950482e00054e4131ef5ae0e49fd98ccb45b
BlairABlake <k111ryousuke@gmail.com>
2023-04-24 22:07:09 +0900

    add batch normalization

--- FNNwithNorm.py
+++ FNNwithNorm.py
@@ -9,25 +9,29 @@ class FNNwithNorm(nn.Module):
 
         self.input_num = input_num
         self.model_f = nn.Sequential(
+            nn.BatchNorm1d(num_features=input_num),
             nn.Linear(in_features=input_num, out_features=128),
             nn.ReLU(),
+            nn.BatchNorm1d(num_features=128),
             nn.Linear(in_features=128, out_features=1024),
             nn.ReLU(),
+            nn.BatchNorm1d(num_features=1024),
             nn.Linear(in_features=1024, out_features=128),
             nn.ReLU(),
             nn.Linear(in_features=128, out_features=1),
-            nn.Tanh()
         )
 
         self.model_i = nn.Sequential(
+            nn.BatchNorm1d(num_features=input_num),
             nn.Linear(in_features=input_num, out_features=128),
             nn.ReLU(),
+            nn.BatchNorm1d(num_features=128),
             nn.Linear(in_features=128, out_features=1024),
             nn.ReLU(),
+            nn.BatchNorm1d(num_features=1024),
             nn.Linear(in_features=1024, out_features=128),
             nn.ReLU(),
             nn.Linear(in_features=128, out_features=1),
-            nn.ReLU()
         )
     
     def forward(self, x):
@@ -35,9 +39,14 @@ class FNNwithNorm(nn.Module):
         x_f = self.model_f(torch.div(x, s))
         x_i = self.model_i(x)
         return torch.add(x_f, x_i)
+    
+if torch.cuda.is_available():
+  device = torch.device("cuda")
+else:
+  device = torch.device("cpu")
 
 input_num = 20
-model = FNNwithNorm(input_num).double()
+model = FNNwithNorm(input_num).double().to(device=device)
 dataset = ForexPricePredictionDataset("./data/USDJPY_H1.csv", header=0, data_order="c", input_duration=input_num, output_duration=1, normalize=False)
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
@@ -51,8 +60,8 @@ for epoch in range(epochs):
     for i, data in enumerate(dataloader):
         x, y = data
         optimizer.zero_grad()
-        outputs = model(x)
-        loss = criterion(outputs, y)
+        outputs = model(x.to(device=device))
+        loss = criterion(outputs, y.to(device=device))
         loss.backward()
         optimizer.step()
 
